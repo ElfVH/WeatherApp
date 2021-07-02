ddb63d6880553872bbc0575ac1e3e9c5480049d8
veerut hengnopparatkul <>
2021-07-02 16:49:47 +0700

    test

--- WeatherApp/ViewController/ForcastViewController.swift
+++ WeatherApp/ViewController/ForcastViewController.swift
@@ -9,10 +9,26 @@ import UIKit
 
 class ForcastViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
+}
+extension ForcastViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        
+        return cell
+    }
+    
+    
 }
--- WeatherApp/ViewController/WeatherViewController.swift
+++ WeatherApp/ViewController/WeatherViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+    
 
+    @IBOutlet weak var searchBar: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showSearchBar(_ sender: UIButton) {
+        if searchBar.isHidden {
+            searchBar.isHidden = false
+        } else {
+            searchBar.isHidden = true
+        }
+//        searchBar.isHidden = true
+    }
+    
 }
 
