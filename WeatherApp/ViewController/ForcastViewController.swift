3ce122d2fa9bedf53e73016598057f62108fee5e
Ittipong Kaewmahing <ittipong.Keawmahing@tesco.com>
2021-07-05 15:30:09 +0700

    improve-UI: Fix UI

--- WeatherApp/ViewController/ForcastViewController.swift
+++ WeatherApp/ViewController/ForcastViewController.swift
@@ -33,7 +33,7 @@ extension ForcastViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 180
+        return 160
     }
     
     
